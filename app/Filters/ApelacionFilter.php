e4add1ebaa4a5f86bfb3cd3946d1d5167cde5b4b
juan rojas homestead <carlos3434@hotmail.com>
2019-09-04 16:06:13 +0000

    fix nombre de clase PersonaFilter desde ApelacionFilter

--- app/Filters/ApelacionFilter.php
+++ app/Filters/ApelacionFilter.php
@@ -7,8 +7,8 @@ use App\Filters\AbstractFilter;
 class ApelacionFilter extends AbstractFilter
 {
     protected $filters = [
-        'persona_id'     => Common\PeronaFilter::class,
-        'documento_id'     => Apelacion\DocumentoFilter::class,//ProcesoDisciplinario
-        'fecha_registro'     => Common\FechaRegistroFilter::class,
+        'persona_id'        => Common\PersonaFilter::class,
+        'documento_id'      => Apelacion\DocumentoFilter::class,//ProcesoDisciplinario
+        'fecha_registro'    => Common\FechaRegistroFilter::class,
     ];
 }
