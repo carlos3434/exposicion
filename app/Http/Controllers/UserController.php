7e8d75b926e75b4e7de35d683bb713885d05229f
juan rojas homestead <carlos3434@hotmail.com>
2019-09-22 01:44:48 +0000

    añadiendo limite de precio 0 a 999

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,8 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $request->request->add(['password' => bcrypt(12345678)]);
+        $request->merge(['password' => bcrypt(12345678)]);
+        //$request->request->add(['password' => bcrypt(12345678)]);
         $user = User::create( $request->all() );
         $success['token'] =  $user->createToken('AppName')->accessToken;
         $user->roles()->sync( $request->get('roles') );
--- app/Http/Requests/Invoice.php
+++ app/Http/Requests/Invoice.php
@@ -24,52 +24,22 @@ class Invoice extends FormRequest
      */
     public function rules()
     {
-        /*return [
-            'tipo_documento_pago_id'       => 'required|exists:tipo_documento_pago,id',
-            'serie_id'              => 'required|exists:series,id',
-            'numero'                => 'string',
-            'cliente'               => 'required|json',
-            'invoiceDetail'         => 'required|json',
-            'tipo_moneda'           => 'required|string',
-            'fecha_emision'         => 'required|date_format:Y-m-d',
-            'fecha_vencimiento'     => 'required|date_format:Y-m-d',
-            'tipo_operacion_id'     => 'exists:tipo_operacion,id',
-            'descuento_global'      => 'numeric',
-            'descuento_total'       => 'numeric',
-            'monto_exogerado'       => 'numeric',
-            'monto_inafecta'        => 'numeric',
-            'monto_gravada'         => 'numeric',
-            'monto_gratuito'        => 'numeric',
-            'igv_total'             => 'numeric',
-            'monto_total'           => 'numeric',
-            'empresa_id'            => 'required|exists:empresas,id',
-
-            'cliente.tipo_documento_identidad_id'        => 'required|integer',
-            'cliente.numero_documento_identidad'         => 'required|integer',
-            'cliente.razon_social'                       => 'required|string',
-            'cliente.direccion'                          => 'string',
-            'cliente.telefono'                           => 'string',
-            'cliente.celular'                            => 'string',
-            'cliente.email'                              => 'email',
-        ];*/
         $rules = [
             'tipo_documento_pago_id'       => 'required|exists:tipo_documento_pago,id',
             'serie_id'              => 'required|exists:series,id',
             'numero'                => 'string',
-            //'cliente'               => 'required|json',
-            //'invoiceDetail'         => 'required|json',
             'tipo_moneda'           => 'required|string',
             'fecha_emision'         => 'required|date_format:Y-m-d',
             'fecha_vencimiento'     => 'required|date_format:Y-m-d',
             'tipo_operacion_id'     => 'exists:tipo_operacion,id',
-            'descuento_global'      => 'numeric|between:0,99.99',
-            'descuento_total'       => 'numeric|between:0,99.99',
-            'monto_exogerado'       => 'numeric|between:0,99.99',
-            'monto_inafecta'        => 'numeric|between:0,99.99',
-            'monto_gravada'         => 'numeric|between:0,99.99',
-            'monto_gratuito'        => 'numeric|between:0,99.99',
-            'igv_total'             => 'numeric|between:0,99.99',
-            'monto_total'           => 'numeric|between:0,99.99',
+            'descuento_global'      => 'numeric|between:0,9999.99',
+            'descuento_total'       => 'numeric|between:0,9999.99',
+            'monto_exogerado'       => 'numeric|between:0,9999.99',
+            'monto_inafecta'        => 'numeric|between:0,9999.99',
+            'monto_gravada'         => 'numeric|between:0,9999.99',
+            'monto_gratuito'        => 'numeric|between:0,9999.99',
+            'igv_total'             => 'numeric|between:0,9999.99',
+            'monto_total'           => 'numeric|between:0,9999.99',
             'empresa_id'            => 'required|exists:empresas,id',
 
             'cliente.tipo_documento_identidad_id'        => 'required|integer',
@@ -87,9 +57,9 @@ class Invoice extends FormRequest
             foreach($this->request->get('invoiceDetail') as $key => $val)
             {
                 $rules['invoiceDetail.'.$key.'.descripcion']        = 'required|string';
-                $rules['invoiceDetail.'.$key.'.precio']             = 'required|numeric|between:0,99.99';
+                $rules['invoiceDetail.'.$key.'.precio']             = 'required|numeric|between:0,9999.99';
                 $rules['invoiceDetail.'.$key.'.cantidad']           = 'required|integer';
-                $rules['invoiceDetail.'.$key.'.descuento_linea']    = 'numeric|between:0,99.99';
+                $rules['invoiceDetail.'.$key.'.descuento_linea']    = 'numeric|between:0,9999.99';
                 $rules['invoiceDetail.'.$key.'.concepto_pago_id']   = 'required|exists:concepto_pago,id';
             }
         }
