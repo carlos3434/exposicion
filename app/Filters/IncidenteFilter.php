a6fa77e04448baa405674758b12a61c456a05b07
juan rojas homestead <carlos3434@hotmail.com>
2019-10-05 22:49:28 +0000

    foto

--- app/Filters/IncidenteFilter.php
+++ app/Filters/IncidenteFilter.php
@@ -8,7 +8,7 @@ class IncidenteFilter extends AbstractFilter
 {
     protected $filters = [
         'persona_id'     => Common\PersonaFilter::class,
-        'tipo_incidente'     => Incidente\TipoIncidenteFilter::class,
+        'tipo_incidente_id'     => Incidente\TipoIncidenteFilter::class,
         'fecha_registro'     => Common\FechaRegistroFilter::class,
         'documento'     => Incidente\DocumentoFilter::class,
     ];
