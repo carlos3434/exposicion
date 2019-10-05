8755f983fbe3d2083bad3135a29a0c86c2712324
juan rojas homestead <carlos3434@hotmail.com>
2019-10-05 05:05:26 +0000

    añadiendo campos a persona resource

--- app/Http/Resources/ListaPostulante/PersonaCollection.php
+++ app/Http/Resources/ListaPostulante/PersonaCollection.php
@@ -21,8 +21,17 @@ class PersonaCollection extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->full_name,
+            'id'                            => $this->id,
+            'full_name'                     => $this->full_name,
+            "apellido_paterno"              => $this->apellido_paterno,
+            "apellido_materno"              => $this->apellido_materno,
+            "nombres"                       => $this->nombres,
+            "fecha_nacimiento"              => $this->fecha_nacimiento,
+            'fecha_registro'                => $this->fecha_registro,
+            'numero_documento_identidad'    => $this->numero_documento_identidad,
+
+            "numero_cmvp"                   => $this->numero_cmvp,
+            'created_at'                    => $this->created_at->toDateTimeString(),
         ];
     }
 
