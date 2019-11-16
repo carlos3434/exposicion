ed8cd1938c4ab6de0e7d094671be057615233d8a
juan rojas homestead <carlos3434@hotmail.com>
2019-11-16 16:41:12 +0000

    use FileUploader

--- app/Helpers/UploadFile.php
+++ app/Helpers/UploadFile.php
@@ -7,9 +7,13 @@ class FileUploader
     /** @var Storage */
     private $storage;
 
-    public function __construct(\Illuminate\Support\Facades\Storage $storage)
+    /** @var File */
+    private $fileManager;
+
+    public function __construct(\Illuminate\Support\Facades\Storage $storage , \File $fileManager)
     {
         $this->storage = $storage;
+        $this->fileManager = $fileManager;
     }
 
     public function upload($file, $fileFolder)
@@ -17,7 +21,7 @@ class FileUploader
         //$image_extension = $file->getClientOriginalExtension();
         $fileName = time().'.'.$file->getClientOriginalExtension();
         //\Illuminate\Support\Facades\Storage::put('uploads/'.$fileFolder.'/'.$fileName, \File::get($file), 'public');
-        $this->storage::put('uploads/'.$fileFolder.'/'.$fileName, $file->getContents(), 'public');
+        $this->storage::put('uploads/'.$fileFolder.'/'.$fileName, $this->fileManager::get($file), 'public');
 
         return $fileName;
     }
