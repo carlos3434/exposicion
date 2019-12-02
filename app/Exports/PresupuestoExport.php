8eecb48a5273530ec33302d892c48cf91fd85585
juan rojas homestead <carlos3434@hotmail.com>
2019-12-02 05:19:04 +0000

    formato de reporte de presupuesto

--- app/Exports/PresupuestoExport.php
+++ app/Exports/PresupuestoExport.php
@@ -4,6 +4,9 @@ namespace App\Exports;
 
 use App\Presupuesto;
 use App\TipoConcepto;
+use App\Pago;
+use App\EstadoPago;
+use DB;
 
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -40,6 +43,8 @@ use Maatwebsite\Excel\Events\BeforeWriting;
 use Maatwebsite\Excel\Events\BeforeSheet;
 use Maatwebsite\Excel\Events\AfterSheet;
 
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
 
 use Illuminate\Contracts\View\View;
@@ -48,7 +53,7 @@ use Maatwebsite\Excel\Concerns\FromView;
 
 //class PresupuestoExport implements FromCollection, WithStrictNullComparison, WithEvents
 //class PresupuestoExport implements FromView, WithStrictNullComparison, WithEvents
-class PresupuestoExport implements FromArray, WithStrictNullComparison, WithEvents
+class PresupuestoExport implements FromArray, WithEvents, WithColumnFormatting
 {
     use Exportable;//, RegistersEventListeners;
 
@@ -56,6 +61,35 @@ class PresupuestoExport implements FromArray, WithStrictNullComparison, WithEven
     private $conceptos;
     private $request;
 
+    private $mes = [
+        '1' => 'ENE',
+        '2' => 'FEB',
+        '3' => 'MAR',
+        '4' => 'ABR',
+        '5' => 'MAY',
+        '6' => 'JUN',
+        '7' => 'JUL',
+        '8' => 'AGO',
+        '9' => 'SET',
+        '10' => 'OCT',
+        '11' => 'NOV',
+        '12' => 'DIC',
+    ];
+
+    private $columna = [
+        '1' => 'C',
+        '2' => 'D',
+        '3' => 'E',
+        '4' => 'F',
+        '5' => 'G',
+        '6' => 'H',
+        '7' => 'I',
+        '8' => 'J',
+        '9' => 'K',
+        '10' => 'L',
+        '11' => 'M',
+        '12' => 'N',
+    ];
 
     public function __construct(
         Request $request,
@@ -102,80 +136,131 @@ class PresupuestoExport implements FromArray, WithStrictNullComparison, WithEven
             return $letter;
         }
     }
+    /**
+     * @return array
+     */
+    public function columnFormats(): array
+    {
+        return [
+            'C' => NumberFormat::FORMAT_NUMBER_00,
+            'D' => NumberFormat::FORMAT_NUMBER_00,
+            'E' => NumberFormat::FORMAT_NUMBER_00,
+            'F' => NumberFormat::FORMAT_NUMBER_00,
+            'G' => NumberFormat::FORMAT_NUMBER_00,
+            'H' => NumberFormat::FORMAT_NUMBER_00,
+            'I' => NumberFormat::FORMAT_NUMBER_00,
+            'J' => NumberFormat::FORMAT_NUMBER_00,
+            'K' => NumberFormat::FORMAT_NUMBER_00,
+            'L' => NumberFormat::FORMAT_NUMBER_00,
+            'M' => NumberFormat::FORMAT_NUMBER_00,
+            'N' => NumberFormat::FORMAT_NUMBER_00,
+        ];
+    }
+    public function getMes($index)
+    {
+        return $this->mes[$index];
+    }
+    public function getColumnaMes($index)
+    {
+        return $this->columna[$index];
+    }
+    public function getMeses()
+    {
+        return $this->mes;
+    }
 
     //public static function afterSheet(AfterSheet $event)
     public function registerEvents(): array
     {
         return [
             AfterSheet::class => function(AfterSheet $event) {
-                //$event->sheet->getDelegate()->getRowDimension(37)->setRowHeight(35);
-                $event->sheet->getDefaultRowDimension()->setRowHeight(15);
+                $anio = $this->request->anio;
+                if (!isset($anio)) {
+                    $anio = '2019';
+                }
+                $event->sheet->getStyle('C5:O100')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_00);
+                //$objPHPExcel->getActiveSheet()->getStyle('B4')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_00);
 
+                $event->sheet->getDefaultRowDimension()->setRowHeight(15);
+                $event->sheet->getDefaultColumnDimension()->setWidth(12);
+                $event->sheet->getColumnDimension('B')->setWidth(50);
                 $event->sheet->getParent()->getDefaultStyle()->applyFromArray([
                     'font' => [
                         'name' => 'Calibri',
                         'size' => 8
                     ]
                 ]);
+                $i = 1;
+                $event->sheet->getStyle("A$i:O$i")->getFont()->setSize(11)->setBold(true);
+                $event->sheet->setCellValue('A'.$i, 'COLEGIO MEDICO VETERINARIO')->getStyle('A'.$i)->getFont()->setBold(true);
+                $i++;
+                $event->sheet->getStyle("A$i:O$i")->getFont()->setSize(10)->setBold(true);
+                $event->sheet->getStyle("A$i:O$i")->getAlignment()->setHorizontal(Alignment::VERTICAL_CENTER);
+                $event->sheet->setCellValue('A'.$i, 'PRESUPUESTO ANUAL CMVP');
+                $event->sheet->mergeCells("A$i:O$i");
 
-                $event->sheet->getStyle('A1:O1')->getFont()->setSize(11)->setBold(true);
-                $event->sheet->getStyle('A2:O2')->getFont()->setSize(10)->setBold(true);
-                $event->sheet->getStyle('A2:O2')->getAlignment()->setHorizontal(Alignment::VERTICAL_CENTER);
-                $event->sheet->setCellValue('A1', 'COLEGIO MEDICO VETERINARIO')->getStyle('A1')->getFont()->setBold(true);
-                $event->sheet->setCellValue('A2', 'PRESUPUESTO ANUAL CMVP');
-                $event->sheet->mergeCells('A2:O2');
+                $i++;
+                foreach ($this->getMeses() as $key => $mes) {
+                    $event->sheet->setCellValue($this->getColumnaMes($key).$i, $this->getMes($key) );
+                }
 
-                //$event->sheet->getRowDimension(1)->setRowHeight(20);
-                //$event->sheet->getRowDimension(1)->setRowHeight(30);
+                $event->sheet->setCellValue('O'.$i, 'TOTAL');
+                $i++;
+                $event->sheet->setCellValue('A'.$i, 'INGRESOS')->getStyle('A'.$i)->getFont()->setBold(true);
 
-                $event->sheet->getColumnDimension('B')->setWidth(50);
-
-                $event->sheet->setCellValue('C4', 'ENE');
-                $event->sheet->setCellValue('D4', 'FEB');
-                $event->sheet->setCellValue('E4', 'MAR');
-                $event->sheet->setCellValue('F4', 'ABR');
-                $event->sheet->setCellValue('G4', 'MAY');
-                $event->sheet->setCellValue('H4', 'JUN');
-                $event->sheet->setCellValue('I4', 'JUL');
-                $event->sheet->setCellValue('J4', 'AGO');
-                $event->sheet->setCellValue('K4', 'SET');
-                $event->sheet->setCellValue('L4', 'OCT');
-                $event->sheet->setCellValue('M4', 'NOV');
-                $event->sheet->setCellValue('N4', 'DIC');
-                $event->sheet->setCellValue('O4', 'TOTAL');
-
-                $event->sheet->setCellValue('A5', 'INGRESOS')->getStyle('A5')->getFont()->setBold(true);
-
-                $i = 6;
-                foreach ($this->conceptos->getIngresos() as $key => $concepto) {
+                $i++;
+                //Ingresos
+                foreach ($this->conceptos->getIngresos() as $concepto) {
                     $event->sheet->setCellValue('A'.$i, $concepto->codigo);
                     $event->sheet->setCellValue('B'.$i, $concepto->name);
+                    //recorrer meses
+                    foreach ($this->getMeses() as $key => $mes) {
+                        //recorrer ingresos en BD
+                        $monto = Pago::where('estado_pago_id', EstadoPago::COMPLETADA)
+                        ->where(DB::raw('YEAR(created_at)'), $anio)
+                        ->where(DB::raw('MONTH(created_at)'), $key )
+                        ->where('concepto_id', $concepto->id)
+                        ->sum(DB::raw('FORMAT(monto,2)'));
+                        $event->sheet->setCellValue($this->getColumnaMes($key).$i, $monto );
+                    }
+
                     $i++;
                 }
                 $event->sheet->setCellValue('B'.$i, 'TOTAL RUBRO INGRESOS')->getStyle('B'.$i)->getFont()->setBold(true);
+                //totales por mes
+
                 $i += 2;
 
                 //Egresos
                 $event->sheet->setCellValue('A'.$i, 'EGRESOS')->getStyle('A'.$i)->getFont()->setBold(true);
                 $i++;
-                //$this->tipo_presupuesto
-                //recorrer tipos concepto
                 $tipoConceptos = TipoConcepto::where('id','<>','1')->get();
                 foreach ($tipoConceptos as $key => $tipoConcepto) {
                     $event->sheet->setCellValue('A'.$i, $tipoConcepto->name)->getStyle('A'.$i)->getFont()->setBold(true);
                     $i++;
-                    //dentro de un tipo concepto preguntar que conceptos pertenecen 
                     foreach ($this->conceptos->getEgresosByTipo($tipoConcepto->id) as $key => $concepto) {
-                        //tipo Cocnepto de pago
                         $event->sheet->setCellValue('A'.$i, $concepto->codigo);
                         $event->sheet->setCellValue('B'.$i, $concepto->name);
+
+                        //recorrer meses
+                        foreach ($this->getMeses() as $key => $mes) {
+                            //recorrer ingresos en BD
+                            $monto = Pago::where('estado_pago_id', EstadoPago::COMPLETADA)
+                            ->where(DB::raw('YEAR(created_at)'), $anio)
+                            ->where(DB::raw('MONTH(created_at)'), $key )
+                            ->where('concepto_id', $concepto->id)
+                            ->sum(DB::raw('FORMAT(monto,2)'));
+                            $event->sheet->setCellValue($this->getColumnaMes($key).$i, $monto );
+                        }
+
                         $i++;
                     }
-
                     $event->sheet->setCellValue('B'.$i, 'TOTAL RUBRO: ')->getStyle('B'.$i)->getFont()->setBold(true);
+                    //totales por mes
+
                     $i+=2;
                 }
-                
+
                 $event->sheet->setCellValue('B'.$i, 'TOTAL EGRESOS')->getStyle('B'.$i)->getFont()->setBold(true);
                 $i += 2;
                 $event->sheet->setCellValue('B'.$i, 'RESULTADO NETO')->getStyle('B'.$i)->getFont()->setBold(true);
