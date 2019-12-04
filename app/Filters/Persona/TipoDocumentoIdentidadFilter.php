72c8b23889f29212ffb1082142135d65338fd54a
juan rojas homestead <carlos3434@hotmail.com>
2019-12-04 13:11:26 +0000

    busqueda de persona con RUC

--- app/Filters/Persona/TipoDocumentoIdentidadFilter.php
+++ app/Filters/Persona/TipoDocumentoIdentidadFilter.php
@@ -5,6 +5,7 @@ class TipoDocumentoIdentidadFilter
 {
     public function filter($builder, $value)
     {
-        return $builder->where('tipo_documento_identidad_id', $value);
+        return $builder->where('tipo_documento_identidad_id', $value)
+        ->orWhere('tipo_documento_identidad_id',4);
     }
 }
