d86d67b0b5bae2db483e3f843e19854a20fc2658
juan rojas homestead <carlos3434@hotmail.com>
2019-12-09 21:54:05 +0000

    nuevo reporte de ingresos

--- app/Http/Controllers/Api/IngresosController.php
+++ app/Http/Controllers/Api/IngresosController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers\Api;
-use App\Persona;
+use DB;
+use App\Invoice;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Persona\PersonaExcelCollection;
@@ -20,26 +21,36 @@ class IngresosController extends Controller
      */
     public function reporte(Request $request)
     {
-        $query = Persona::filter($request)
-            ->with([
-                'tipoDocumentoIdentidad',
-                'nacionalidad',
-                'estadoCivil',
-                'departamento',
-                'distrito',
-                'provincia',
-                'universidadProcedencia',
-                'especialidadPosgrado',
-                'areaEjercicioProfesional',
-                'departamentoColegiado',
-                'estadoRegistroColegiado',
-                'estadoCuentaSistema'
-        ]);
-
-        $result = new PersonaExcelCollection( $query->get() );
+        $result = Invoice::select(
+            'invoices.fecha_emision as fecha_pago',
+            's.name as serie',
+            'invoices.numero',
+            'id.valor_venta',
+            'id.descripcion',
+            'per.numero_documento_identidad as numeroDocumento',
+            'per.numero_cmvp as numeroColegiado',
+            DB::raw("concat( per.nombres,' ',per.apellido_paterno , ' ', per.apellido_materno ) as persona")
+        )
+        ->join('series as s','invoices.serie_id','=','s.id')
+        ->join('invoice_detail as id','invoices.id','=','id.invoice_id')
+        ->join('personas as per','invoices.persona_id','=','per.id')
+        ->join('pagos as p','id.pago_id','=','p.id')
+        ->join('ubigeos as u','p.departamento_id','=','u.id')
+        ->where('invoices.is_nota',0)
+        ->where('invoices.cdr_path','<>','')
+        ->when($request->has('departamento_id'), function ($query) use ($request) {
+            return $query->where('p.departamento_id', $request->departamento_id );
+        })
+        ->when($request->has('fecha_inicio'), function ($query) use ($request)  {
+            return $query->where('invoices.fecha_emision','>=', $request->fecha_inicio );
+        })
+        ->when($request->has('fecha_fin'), function ($query) use ($request)  {
+            return $query->where('invoices.fecha_emision','<=', $request->fecha_fin );
+        })
+        ->get();
 
         return $result->downloadExcel(
-            'registros_'.date('m-d-Y_hia').'.xlsx',
+            'ingresos_'.date('m-d-Y_hia').'.xlsx',
             $writerType = null,
             $headings = true
         );
